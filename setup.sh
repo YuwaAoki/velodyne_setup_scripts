ede2cd61dee36571df0ce27301f2eef68cbf3f2c
aoki <yuwablue98@gmail.com>
2022-06-10 10:58:21 +0900

    fix

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 chmod +x connect_velodyne.sh
-cp connect_velodyne.service /etc/systemd/system
+cp ./connect_velodyne.service /etc/systemd/system
 systemctl daemon-reload
 systemctl status connect_velodyne.service
 systemctl enable connect_velodyne.service
